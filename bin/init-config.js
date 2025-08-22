1fca60e101678a2730b3c24757d9e5b0838508ee
grimlink <sean.grimlink@gmail.com>
2025-08-22 19:33:36 +0200

    ADD: init action

--- bin/init-config.js
+++ bin/init-config.js
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+import fs from "node:fs/promises";
+import { constants } from "node:fs";
+import path from "node:path";
+import { cwd, exit } from "node:process";
+import {
+    consoleError,
+    consoleWarn,
+    consoleSuccess,
+} from "../src/utils/index.js";
+
 const sampleConfig = {
     tailwind: {
         include: [
@@ -14,7 +24,28 @@ const sampleConfig = {
     },
 };
 
-fs.writeFile(
-    path.join(cwd(), "hyva.config.json"),
-    JSON.stringify(sampleConfig, null, 2)
-);
+const configFilePath = path.join(cwd(), "hyva.config.json");
+
+(async () => {
+    try {
+        await fs.access(configFilePath, constants.F_OK);
+        consoleWarn("hyva.config.json already exists. Exiting.");
+        exit(0);
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            try {
+                await fs.writeFile(
+                    configFilePath,
+                    JSON.stringify(sampleConfig, null, 2)
+                );
+                consoleSuccess("hyva.config.json created successfully.");
+            } catch (writeError) {
+                consoleError(`Failed to write hyva.config.json: ${writeError}`);
+                exit(1);
+            }
+        } else {
+            consoleError(`Error checking for hyva.config.json: ${error}`);
+            exit(1);
+        }
+    }
+})();
